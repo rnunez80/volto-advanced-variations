705fb9253177481b4947959530358ac35651ca9e
rnunez <rnunez@york.cuny.edu>
2024-08-08 16:06:09 -0400

    rolled back

--- src/ResponsiveImage.jsx
+++ src/ResponsiveImage.jsx
@@ -1,57 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Image } from 'semantic-ui-react';
 import { flattenToAppURL } from '@plone/volto/helpers';
 
-const getSrcSet = (imageUrl) => `
-    ${imageUrl}/mini 200w,
-    ${imageUrl}/preview 400w,
-    ${imageUrl}/teaser 600w,
-    ${imageUrl}/large 800w,
-    ${imageUrl}/larger 1000w,
-    ${imageUrl}/great 1200w,
-    ${imageUrl}/huge 1600w
-`;
-
-const getSizes = (howManyColumns) => {
-  switch (howManyColumns) {
-    case 2:
-      return '(min-width: 768px) and (max-width: 991px) 355px, (min-width: 992px) and (max-width: 1199px) 460px, (min-width: 1200px) 557px';
-    case 3:
-      return '(min-width: 768px) and (max-width: 991px) 232px, (min-width: 992px) and (max-width: 1199px) 302px, (min-width: 1200px) 367px';
-    case 4:
-      return '(min-width: 768px) and (max-width: 991px) 171px, (min-width: 992px) and (max-width: 1199px) 223px, (min-width: 1200px) 272px';
-    default:
-      return '(min-width: 768px) and (max-width: 991px) 1000px, (min-width: 992px) and (max-width: 1199px) 1200px';
-    }
-};
-
-const ResponsiveImage = React.memo(({ item, howManyColumns }) => {
+const ResponsiveImage = ({ item, howManyColumns }) => {
   const imageUrl = flattenToAppURL(`${item.url}/@@images/${item.image_field}`);
-  const srcset = getSrcSet(imageUrl);
-  const sizes = getSizes(howManyColumns);
+  let srcset = `
+        ${imageUrl}/mini 200w,
+        ${imageUrl}/preview 400w,
+        ${imageUrl}/teaser 600w,
+        ${imageUrl}/large 800w,
+        ${imageUrl}/larger 1000w,
+        ${imageUrl}/great 1200w,
+        ${imageUrl}/huge 1600w
+    `;
+
+  let sizes = '(min-width: 768px) and (max-width: 991px) 1000px, (min-width: 992px) and (max-width: 1199px) 1200px';
+
+  if (howManyColumns === 2) {
+    sizes = '(min-width: 768px) and (max-width: 991px) 355px, (min-width: 992px) and (max-width: 1199px) 460px, (min-width: 1200px) 557px';
+  }
+  if (howManyColumns === 3) {
+    sizes = '(min-width: 768px) and (max-width: 991px) 232px, (min-width: 992px) and (max-width: 1199px) 302px, (min-width: 1200px) 367px';
+  }
+  if (howManyColumns === 4) {
+    sizes = '(min-width: 768px) and (max-width: 991px) 171px, (min-width: 992px) and (max-width: 1199px) 223px, (min-width: 1200px) 272px';
+  }
 
     return (
         <Image
             className='listImage'
-            srcSet={srcset}
+            srcset={srcset}
             sizes={sizes}
-            alt={item.title || 'Image'}
+            alt={item.title}
             size='small'
             width='100%'
             src={imageUrl + '/preview'}
-            loading='lazy'
         />
     );
-});
-
-ResponsiveImage.propTypes = {
-  item: PropTypes.shape({
-    url: PropTypes.string.isRequired,
-    image_field: PropTypes.string.isRequired,
-    title: PropTypes.string,
-  }).isRequired,
-  howManyColumns: PropTypes.number,
 };
 
 export default ResponsiveImage;
--- src/renderImage.jsx
+++ src/renderImage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import DefaultImageSVG from './placeholder.png';
 import { Image } from 'semantic-ui-react';
 import { useIntl } from 'react-intl';
@@ -7,36 +6,28 @@ import messages from './messages';
 import ResponsiveImage from './ResponsiveImage';
 import { Link } from 'react-router-dom';
 
-const RenderImage = ({ item, isEditMode, howManyColumns }) => {
+const renderImage = (item, isEditMode, howManyColumns) => {
   const intl = useIntl();
 
-  const imageAltText = intl.formatMessage(messages.thisContentHasNoImage);
-
-  return (
-    <Link to={item.url} condition={!isEditMode}>
-      {item.image_field ? (
-        <ResponsiveImage item={item} howManyColumns={howManyColumns} />
-      ) : (
+  if (!item.image_field) {
+    return (
+      <Link to={item.url} condition={!isEditMode}>
         <Image
           className='listImage'
           src={DefaultImageSVG}
-          alt={imageAltText}
+          alt={intl.formatMessage(messages.thisContentHasNoImage)}
           size='small'
           width='100%'
         />
-      )}
+      </Link>
+    );
+  }
+
+  return (
+    <Link to={item.url} condition={!isEditMode}>
+      <ResponsiveImage item={item} howManyColumns={howManyColumns} />
     </Link>
   );
 };
 
-RenderImage.propTypes = {
-  item: PropTypes.shape({
-    url: PropTypes.string.isRequired,
-    image_field: PropTypes.string,
-  }).isRequired,
-  isEditMode: PropTypes.bool,
-  howManyColumns: PropTypes.number,
-};
-
-export { RenderImage };
-export default React.memo(RenderImage);
+export default renderImage;
