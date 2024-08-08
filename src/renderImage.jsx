1fa55b689213c69848d26cfcb1519b517f7848d7
rnunez <rnunez@york.cuny.edu>
2024-08-08 17:06:00 -0400

    working version

--- src/renderImage.jsx
+++ src/renderImage.jsx
@@ -1,48 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import DefaultImageSVG from './placeholder.png';
 import { Image } from 'semantic-ui-react';
-import { flattenToAppURL } from '@plone/volto/helpers';
+import { useIntl } from 'react-intl';
+import messages from './messages';
+import ResponsiveImage from './ResponsiveImage';
+import { Link } from 'react-router-dom';
 
-const getSrcSet = (imageUrl) => `
-    ${imageUrl}/mini 200w,
-    ${imageUrl}/preview 400w,
-    ${imageUrl}/teaser 600w,
-    ${imageUrl}/large 800w,
-    ${imageUrl}/larger 1000w,
-    ${imageUrl}/great 1200w,
-    ${imageUrl}/huge 1600w
-`;
+const renderImage = (item, isEditMode, howManyColumns) => {
+  const intl = useIntl();
 
-const getSizes = (howManyColumns) => {
-  switch (howManyColumns) {
-    case 2:
-      return '(min-width: 768px) and (max-width: 991px) 355px, (min-width: 992px) and (max-width: 1199px) 460px, (min-width: 1200px) 557px';
-    case 3:
-      return '(min-width: 768px) and (max-width: 991px) 232px, (min-width: 992px) and (max-width: 1199px) 302px, (min-width: 1200px) 367px';
-    case 4:
-      return '(min-width: 768px) and (max-width: 991px) 171px, (min-width: 992px) and (max-width: 1199px) 223px, (min-width: 1200px) 272px';
-    default:
-      return '(min-width: 768px) and (max-width: 991px) 1000px, 100vw';
+  if (!item.image_field) {
+    return (
+      <Link to={item.url} condition={!isEditMode}>
+        <Image
+          className='listImage'
+          src={DefaultImageSVG}
+          alt={intl.formatMessage(messages.thisContentHasNoImage)}
+          size='small'
+          width='100%'
+        />
+      </Link>
+    );
   }
-};
 
-const renderImage = ({ item, isEditMode, howManyColumns }) => {
-  const imageUrl = flattenToAppURL(item.image?.scales?.great?.download || item.image);
   return (
-    <Image
-      src={imageUrl}
-      srcSet={getSrcSet(imageUrl)}
-      sizes={getSizes(howManyColumns)}
-      alt={item.title}
-      style={isEditMode ? { pointerEvents: 'none' } : {}}
-    />
+    <Link to={item.url} condition={!isEditMode}>
+      <ResponsiveImage item={item} howManyColumns={howManyColumns} />
+    </Link>
   );
-};
-
-renderImage.propTypes = {
-  item: PropTypes.object.isRequired,
-  isEditMode: PropTypes.bool,
-  howManyColumns: PropTypes.number,
 };
 
 export default renderImage;
