2168175e6d211179732cbb59f9ebdc9241116b12
rnunez <rnunez@york.cuny.edu>
2024-08-19 15:23:59 -0400

    adjusted image size to improve pagespeed: Serve images that are appropriately-sized to save cellular data and improve load time.

--- src/ResponsiveImage.jsx
+++ src/ResponsiveImage.jsx
@@ -4,13 +4,13 @@ import { Image } from 'semantic-ui-react';
 import { flattenToAppURL } from '@plone/volto/helpers';
 
 const getSrcSet = (imageUrl) => `
-    ${imageUrl}/mini 200w,
-    ${imageUrl}/preview 400w,
-    ${imageUrl}/teaser 600w,
-    ${imageUrl}/large 800w,
-    ${imageUrl}/larger 1000w,
-    ${imageUrl}/great 1200w,
-    ${imageUrl}/huge 1600w
+        ${imageUrl}/mini 200w,
+        ${imageUrl}/preview 600w,
+        ${imageUrl}/teaser 800w,
+        ${imageUrl}/large 1000w,
+        ${imageUrl}/larger 1200w,
+        ${imageUrl}/great 1400w,
+        ${imageUrl}/huge 1600w
 `;
 
 const getSizes = (howManyColumns) => {
@@ -22,7 +22,7 @@ const getSizes = (howManyColumns) => {
     case 4:
       return '(min-width: 768px) and (max-width: 991px) 171px, (min-width: 992px) and (max-width: 1199px) 223px, (min-width: 1200px) 272px';
     default:
-      return '(min-width: 768px) and (max-width: 991px) 1000px, (min-width: 992px) and (max-width: 1199px) 1200px';
+      return '(min-width: 200px) and (max-width: 400px) 200px,(min-width: 401px) and (max-width: 767px) 600px, (min-width: 768px) and (max-width: 991px) 800px, (min-width: 992px) and (max-width: 1199px) 1000px';
   }
 };
 
@@ -39,7 +39,7 @@ const ResponsiveImage = React.memo(({ item, howManyColumns }) => {
             alt={item.title || 'Image'}
             width='100%'
             src={imageUrl + '/preview'}
-            loading='lazy'
+            loading='eager'
         />
     );
 });
